2d9ecc11ed8b1c8e7e3d234b291bd9156a5afd82
nevelson <nevelson92@gmail.com>
2020-06-08 15:32:42 -0400

    Fib

--- drills.js
+++ drills.js
@@ -60,10 +60,17 @@
 
 //Drill 6 ---------------------------------------------------------
 // function fibonacci(n) {
-//   if (n < 1) {
+//   if(n <= 0) {
+//     return 0;
+//   }
+
+//   if (n < 2) {
 //     return 1;
 //   }
+
 //   return fibonacci(n - 1) + fibonacci(n - 2);
 // }
 
-// console.log(fibonacci(5));
+// console.log(fibonacci(7));
+
+//Drill 7 ---------------------------------------------------------
