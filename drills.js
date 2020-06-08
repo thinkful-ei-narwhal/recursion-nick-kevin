08e06884947b61fb06c67d56174882d74f629f4d
nevelson <nevelson92@gmail.com>
2020-06-08 12:32:54 -0400

    Added drill 2

--- drills.js
+++ drills.js
@@ -1,13 +1,27 @@
 // Drill 1 ------------------------------------------------------
 let sheepCounter = (sheep) => {
-  if(sheep < 1) {
-    console.log('All sheep jumped over the fence');
-    return;
+  if (sheep < 1) {
+    return "All sheep jumped over the fence";
   }
   console.log(`${sheep}: Another sheep jumps over the fence`);
-  return sheepCounter(sheep -1 );
+  return sheepCounter(sheep - 1);
 };
 
 console.log(sheepCounter(5));
 
 // Drill 2 ------------------------------------------------------
+function powerCalculator(base, exponent) {
+  if (exponent < 1) {
+    console.log("Exponent should be >= 0");
+  }
+
+  if (exponent === 1) {
+    return base;
+  }
+
+  return base * powerCalculator(base, exponent - 1);
+}
+
+console.log(powerCalculator(10, 2));
+
+//Drill 3 -------------------------------------------------------
